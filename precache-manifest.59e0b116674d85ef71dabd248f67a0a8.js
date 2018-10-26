f1a8689187aed9fe14d8fc00ab79f78c8eb19d64
Hashemi Rafsan <rafsanhashemi@gmail.com>
2018-10-26 11:37:02 +0600

    Updates

---
+++ precache-manifest.59e0b116674d85ef71dabd248f67a0a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e6f4508bd14f2312f0f",
+    "url": "/static/js/main.2e6f4508.chunk.js"
+  },
+  {
+    "revision": "a1c19c2ec84221fa3dd3",
+    "url": "/static/js/1.a1c19c2e.chunk.js"
+  },
+  {
+    "revision": "2e6f4508bd14f2312f0f",
+    "url": "/static/css/main.b2cbf898.chunk.css"
+  },
+  {
+    "revision": "a1c19c2ec84221fa3dd3",
+    "url": "/static/css/1.ea529a75.chunk.css"
+  },
+  {
+    "revision": "722ff178a3ed74a2773e957d249bac4e",
+    "url": "/index.html"
+  }
+];
